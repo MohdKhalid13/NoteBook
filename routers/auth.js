07cae4980a47703961b04773b124b82a4277955a
MohdKhalid13 <mohdkhalid.mansoori@gmail.com>
2023-02-18 16:49:58 +0530

    Secondary Commit

--- routers/auth.js
+++ routers/auth.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req,res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+
+router.post('/',[
+body('email',"Enter a Valid Email").isEmail(),
+body('name',"Enter a Valid name").isLength({ min: 5 }),
+body('password',"Enter a Valid Password").isLength({ min: 5 }),
+],(req,res) => {
+ 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+      .catch(err => {console.log(err)
+        res.json({error : 'Please Enter a Valid Email',message:err.message})
+      })
 })
 
 module.exports = router
