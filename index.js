4b4b643e47c7516460eb97774bbcc77efda77aa3
MohdKhalid13 <mohdkhalid.mansoori@gmail.com>
2023-02-18 15:42:52 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require('./db');
+const express = require('express');
+
+connectToMongo();
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+// app.post('/',(req,res) => {
+//   res.send(req.body)
+// })
+
+app.use('/api/auth', require('./routers/auth'));
+app.use('/api/notes', require('./routers/notes'))
+
+app.listen(PORT , () => {
+  console.log(`The BackEnd Is Running On Port ${PORT}`);
+})
